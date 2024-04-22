ed17c23c236cc362a51fe10d0fe77669f4556074
Cyrus Mante <74973194+mattekudacy@users.noreply.github.com>
2024-04-23 02:13:15 +0800

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -5,10 +5,11 @@ import streamlit as st
 st.sidebar.title("Try other models:")
 
 with st.sidebar:
-    st.link_button("Text Summarizer 📚", "https://simple-nlp.herokuapp.com/")
-    # st.link_button("Home", "https://simple-nlp.herokuapp.com/")
-    # st.link_button("Home", "https://simple-nlp.herokuapp.com/")
-    # st.link_button("Home", "https://simple-nlp.herokuapp.com/")
+    st.link_button("Text Summarizer 📚", "https://nlp-made-easy-summarize.streamlit.app/")
+    st.link_button("Question Answering System ❓", "https://simple-nlp.herokuapp.com/")
+    st.link_button("Text Generator (GPT-2) 📝", "https://simple-nlp.herokuapp.com/")
+    st.link_button("Text Translator (English to French) 🇫🇷", "https://simple-nlp.herokuapp.com/")
+    st.link_button("Sentiment Analysis 😊", "https://simple-nlp.herokuapp.com/")
 
 st.title("Text Summarizer")
 st.write("This app summarizes the input text using Hugging Face's transformers library. It uses the pipeline API for summarization.")
